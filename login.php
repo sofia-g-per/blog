a3055ef3d5ab2baa463b589718fcaeed0656f8a7
Sofia <sofia.g.per@gmail.com>
2021-06-02 12:23:53 +0300

    login completed

--- login.php
+++ login.php
@@ -1,7 +1,53 @@
 <?php
 require_once("core/helpers.php");
+require_once("core/init.php");
 
-$loginContent = include_template("pages/login-template.php");
+$errors = [];
+$rules = [
+    'email' => function(){
+        return validateEmail('email');
+    },
+    'password' => function(){
+        return validateFilled('password');
+    }
+];
+
+$user = $_POST;
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    foreach($user as $key => $value){
+        if(isset($rules[$key])){
+            $errors[$key] = $rules[$key]();
+        }
+    }
+}
+$errors = array_filter($errors);
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && empty($errors)){
+    $stmnt = $con -> prepare('SELECT * FROM Users WHERE email=:email');
+    $stmnt -> execute(['email' => $user['email']]);
+
+    if($stmnt -> rowCount() == 0){
+        $errors['email'] = "Пользователь с данным email адресом не зарегистрирован";
+    } else{
+        $dbUser = $stmnt -> fetch();
+
+        if(password_verify($user['password'], $dbUser['password'])){
+            $_SESSION['user_id'] = $dbUser['id'];
+            $_SESSION['login'] = $dbUser['login'];
+            header("Location: popular.php");
+        }
+        else{
+            //если неверный пароль
+            $errors['password'] = "Введите корректные данные";
+        }
+    }
+}
+
+$loginContent = include_template("pages/login-template.php", [
+    "errors" => $errors
+]);
 
 $page = include_template("login-layout.php", [
     "content" => $loginContent
--- templates/pages/login-template.php
+++ templates/pages/login-template.php
@@ -7,23 +7,23 @@
         <form class="login__form form" action="#" method="post">
           <div class="login__input-wrapper form__input-wrapper">
             <label class="login__label form__label" for="login-email">Электронная почта</label>
-            <div class="form__input-section">
-              <input class="login__input form__input" id="login-email" type="email" name="email" placeholder="Укажите эл.почту">
+            <div class="form__input-section <?= isset($errors['email']) ? "form__input-section--error" : "" ?>">
+              <input class="login__input form__input" id="login-email" type="email" name="email" placeholder="Укажите эл.почту" value = "<?= getPostVal('email'); ?>"> 
               <button class="form__error-button button" type="button">!<span class="visually-hidden">Информация об ошибке</span></button>
               <div class="form__error-text">
                 <h3 class="form__error-title">Заголовок сообщения</h3>
-                <p class="form__error-desc">Текст сообщения об ошибке, подробно объясняющий, что не так.</p>
+                <p class="form__error-desc"><?= $errors['email']?></p>
               </div>
             </div>
           </div>
           <div class="login__input-wrapper form__input-wrapper">
             <label class="login__label form__label" for="login-password">Пароль</label>
-            <div class="form__input-section">
+            <div class="form__input-section <?= isset($errors['password']) ? "form__input-section--error" : "" ?>">
               <input class="login__input form__input" id="login-password" type="password" name="password" placeholder="Введите пароль">
               <button class="form__error-button button button--main" type="button">!<span class="visually-hidden">Информация об ошибке</span></button>
               <div class="form__error-text">
                 <h3 class="form__error-title">Заголовок сообщения</h3>
-                <p class="form__error-desc">Текст сообщения об ошибке, подробно объясняющий, что не так.</p>
+                <p class="form__error-desc"><?= $errors['password']?></p>
               </div>
             </div>
           </div>
